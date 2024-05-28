4408d45148e6a0f44be556d81b808ddcc6bb2d1e
Kristoffer Frank <kfr@vertica.dk>
2024-05-28 15:55:25 +0200

    Initial Commit

---
+++ DiscBag/DiscBagApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiscBagApp.swift
+//  DiscBag
+//
+//  Created by Kristoffer Frank on 28/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct DiscBagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
